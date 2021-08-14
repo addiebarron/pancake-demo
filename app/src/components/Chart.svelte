89656c903ad471a2230336d0ca8b7a436d772f3c
Addie <addiebarronemail@gmail.com>
2021-08-14 11:10:30 -0400

    noJS iframe rendering working

--- app/src/components/Chart.svelte
+++ app/src/components/Chart.svelte
@@ -1,8 +1,7 @@
 <script>
   import * as Pancake from "@sveltejs/pancake";
 
-  
-  // Server side API request
+  export let zip;
 </script>
 
-
+<p>{zip}</p>
--- app/src/routes/viz.svelte
+++ app/src/routes/viz.svelte
@@ -1,15 +1,18 @@
-<script>
-  import { page } from "$app/stores";
-
-  import Chart from "../components/Chart.svelte";
-
-  let zip;
-
-  if (page.query.has("zip")) {
-    zip = page.query.get("zip");
-  } else {
-    zip = 19143;
+<script context="module">
+  export async function load({ page }) {
+    let zip = 19143;
+    if (page.query.has("zip")) {
+      zip = page.query.get("zip");
+    }
+    return { props: { zip } };
   }
 </script>
 
+<script>
+  import Chart from "../components/Chart.svelte";
+
+  export let zip;
+</script>
+
+<h1>Chart</h1>
 <Chart {zip} />
