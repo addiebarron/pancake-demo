519f50a79ac56ec37adc7a4f16490e8954c6329f
Addie <addiebarronemail@gmail.com>
2021-08-17 16:07:11 -0400

    Fix background style issue

--- app/src/routes/__error.svelte
+++ app/src/routes/__error.svelte
@@ -8,15 +8,19 @@
   export let status;
 </script>
 
-<h1>{status}</h1>
-<p>
-  {#if status == 500}There's probably no data in the database.{/if}
-  File an issue
-  <a href="https://github.com/addiebarron/covid-viz/issues">here</a>.
-</p>
+<main>
+  <h1>{status}</h1>
+  <p>
+    {#if status == 500}There's probably no data in the database.{/if}
+    File an issue
+    <a href="https://github.com/addiebarron/covid-viz/issues">here</a>.
+  </p>
+</main>
 
 <style>
-  :global(body) {
+  main {
+    width: 100%;
+    height: 100%;
     background-color: white;
   }
 </style>
